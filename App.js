8808b3fd6d9b6f1747fe085a9ccfc5d7dfa449a4
YarnoVdW <yarnovandeweyer2@hotmail.com>
2023-04-05 10:40:22 +0200

    Task: Git setup

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import {NativeBaseProvider, Box, View, Heading, HStack, Stack, Center, AspectRatio, Container} from "native-base";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+
+
+    </NativeBaseProvider>
   );
 }
 
@@ -17,4 +18,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  boxTest: {
+    flex:1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
