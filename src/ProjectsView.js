8941d636366331bb8c357b009e415fd58851dd62
YarnoVdW <yarnovandeweyer2@hotmail.com>
2023-04-05 13:52:19 +0200

    Feature: added github projects using fetch api

--- src/ProjectsView.js
+++ src/ProjectsView.js
@@ -1,40 +1,75 @@
-import {Text} from "react-native";
+import {Pressable, Text} from "react-native";
 import {Avatar, Box, FlatList, Heading, HStack, Spacer, VStack} from "native-base";
+import {useEffect, useState} from "react";
 
-export const Projects =() =>{
-    const data = fetch("https://api.github.com/users/yarnoVdW/repos");
-    return (
-     <Box>
-        <Heading fontSize="xl" p="4" pb="3">
-            Projects
-        </Heading>
-        <FlatList  renderItem={({
-                                               item
-                                           }) => <Box borderBottomWidth="1" _dark={{
-            borderColor: "muted.50"
-        }} borderColor="muted.800" pl={["0", "4"]} pr={["0", "5"]} py="2">
-            <HStack space={[2, 3]} justifyContent="space-between">
 
-                <VStack>
-                    <Text _dark={{
-                        color: "warmGray.50"
-                    }} color="coolGray.800" bold>
-                        {item.fullName}
-                    </Text>
-                    <Text color="coolGray.600" _dark={{
-                        color: "warmGray.200"
-                    }}>
-                        "test"
-                    </Text>
-                </VStack>
-                <Spacer />
-                <Text fontSize="xs" _dark={{
-                    color: "warmGray.50"
-                }} color="coolGray.800" alignSelf="flex-start">
-                    {item.timeStamp}
-                </Text>
-            </HStack>
-        </Box>} keyExtractor={item => item.id} />
-    </Box>
-    );
+async function fetchProjects() {
+    try {
+        const response = await fetch("https://api.github.com/users/yarnoVdW/repos");
+        const data = (await response).json();
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
 }
+export const Projects =() =>{
+    const [projects, setProjects] = useState([]);
+    useEffect(() => {
+        async function getProjects() {
+            const data = await fetchProjects();
+            setProjects(data);
+        }
+        getProjects();
+    }, []);
+    const renderProjectItem = ({ item }) => (
+        <Box
+            borderWidth={1}
+            borderColor="gray.200"
+            borderRadius={4}
+            overflow="hidden"
+            shadow={"lg"}
+            mb={4}
+            mx={2}
+        >
+            <Pressable onPress={() => console.log(item.name)}>
+                <Box p={4}>
+                    <VStack space={2}>
+                        <Heading size="md" isTruncated>
+                            {item.name}
+                        </Heading>
+                        <Text color="coolGray.600" isTruncated>
+                            {item.description == null? "" : item.description.length > 50
+                                ? `${item.description.slice(0, 50)}...`
+                                : item.description}
+                        </Text>
+                        <HStack justifyContent="space-between">
+                            <Text fontSize="xs" color="coolGray.500">
+                                {item.timeStamp}
+                            </Text>
+                            {(
+                                <Pressable onPress={() => console.log(item.description)}>
+                                    <Text fontSize="xs" color="primary.500">
+                                        Read More
+                                    </Text>
+                                </Pressable>
+                            )}
+                        </HStack>
+                    </VStack>
+                </Box>
+            </Pressable>
+        </Box>
+    );
+
+    return (
+        <Box>
+            <Heading fontSize="xl" p={4} pb={3}>
+                Projects
+            </Heading>
+            <FlatList
+                data={projects}
+                renderItem={renderProjectItem}
+                keyExtractor={(item) => item.id.toString()}
+            />
+        </Box>
+    );
+};
